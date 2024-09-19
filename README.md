ae16183a3277bfcfa88642b64340b71183ff930e
joemphilips <joemphilips@gmail.com>
2024-09-19 21:26:00 +0900

    update

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-
+# Bitcoin Tokyo 2024 Technical Workshop
 
 ## Instrucitons
 
 * [English](https://docs.google.com/document/d/1uocAbHHI6CrIe6MWa6gdhoZLjpmh5HAsHUl0j6q9rKE/edit?usp=sharing)
 * [日本語](https://docs.google.com/document/d/1okmvrc6Ll5USOApr1dtPRuQNoY_ppC5KyYURb35qmi0/edit?usp=sharing)
 
+## フォルダ概要
 
+* `Dockerfile` ... sparrow をビルドする例となる Dockerfile
+* `docker_for_bdkcli/` ... `bdk-cli` をテストする際の docker compose 及びスクリプト
--- docker_for_bdkcli/bdk-cli-example.sh
+++ docker_for_bdkcli/bdk-cli-example.sh
@@ -4,7 +4,6 @@ set -eux
 
 WALLET_NAME=alice
 
-
 # xprv is created by
 # bdk-cli --network=regtest key generate | jq .xprv | xargs -IXX bdk-cli key derive --xprv XX --path='m/84'/1'/0'/0'' | jq -r .xprv
 xprv="[f28584ff/84/1/0/0]tprv8i7N25RiNQiRHptaDDZQbKSjnxNmQBicA2GSort2zTTBvQEMSSafTqu6NBj7SShX1wMdYPxpRKZzj2sBAdqyc9jJhV3qY2W8PHnhBdZ4721/*"
@@ -13,9 +12,9 @@ bdk_wallet(){
   bdk-cli --network=regtest wallet --descriptor="wpkh($xprv)" $@
 }
 
-./bitcoin-cli.sh createwallet $WALLET_NAME
-bitcoind_address=$(./bitcoin-cli.sh -rpcwallet=$WALLET_NAME getnewaddress)
-./bitcoin-cli.sh generatetoaddress 101 $bitcoind_address
+# ./bitcoin-cli.sh createwallet $WALLET_NAME
+# bitcoind_address=$(./bitcoin-cli.sh -rpcwallet=$WALLET_NAME getnewaddress)
+# ./bitcoin-cli.sh generatetoaddress 101 $bitcoind_address
 
 bdk_address=$(bdk_wallet get_new_address | jq -r ".address")
 ./bitcoin-cli.sh -rpcwallet=$WALLET_NAME sendtoaddress  $bdk_address 5
